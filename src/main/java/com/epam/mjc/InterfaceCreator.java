3ead3a39de07d0ec09501570eb2aab9d454f66e0
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-07-13 14:13:56 +0300

    Completed lambda task.

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class InterfaceCreator {
 
-    public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    public Operation<Integer> divideBy(Integer divider) { return x -> {
+        List<Integer> result = new LinkedList<>();
+        for (Integer e: x) {
+            result.add(e/ divider);
+        }
+        return result;
+    };
+
     }
 }
